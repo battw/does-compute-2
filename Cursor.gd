a69667db93fa44d66644fd2371158debefc43a82
Ben Attwood <bendattwood@gmail.com>
2019-05-29 00:52:29 +0000

    Changed cursor colors

--- Cursor.gd
+++ Cursor.gd
@@ -6,6 +6,10 @@ func _process(delta):
 	self.position = get_viewport().get_mouse_position() - get_viewport().canvas_transform.get_origin()
 
 func set_mode(mode):
+	if !$Add or !$Invert or !$Delete:
+		print("Cursor missing children")
+		return
+		
 	$Add.visible = false
 	$Delete.visible = false
 	$Invert.visible = false
